c48ef1b29844c76ed652dc6747f7daa49f6d573f
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-08-27 16:40:06 +0700

    create dto for email

--- src/main/java/com/nguyenvu/lopet/email/EmailController.java
+++ src/main/java/com/nguyenvu/lopet/email/EmailController.java
@@ -1,15 +1,18 @@
 package com.nguyenvu.lopet.email;
 
-import com.nguyenvu.lopet.common.response.ApiResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.nguyenvu.lopet.common.response.ApiResponse;
+import com.nguyenvu.lopet.email.dto.SendOtpRequest;
+import com.nguyenvu.lopet.email.dto.VerifyOtpRequest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Email management", description = "APIs used for sending OTP, OTP Verify)")
 @RestController
@@ -21,15 +24,15 @@ public class EmailController {
 
     @Operation(summary = "Send OTP into email provided")
     @PostMapping
-    public ApiResponse<Void> sendOtp(@RequestBody Map<String, String> body) {
-        emailService.sendOtp(body.get("email"));
+    public ApiResponse<Void> sendOtp(@Valid @RequestBody SendOtpRequest request) {
+        emailService.sendOtp(request.email());
         return ApiResponse.message(200, "Send email successfully");
     }
 
     @Operation(summary = "Verify OTP that sent into email provided")
     @PostMapping("/verify")
-    public ApiResponse<Void> verifyOtp(@RequestBody Map<String, String> body) {
-        emailService.verify(body.get("email"), body.get("otp"));
+    public ApiResponse<Void> verifyOtp(@Valid @RequestBody VerifyOtpRequest request) {
+        emailService.verify(request.email(), request.otp());
         return ApiResponse.message(200, "Verify email successfully");
     }
 }
